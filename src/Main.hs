c13b0f6d0838fc12cb74485f4e0a8df2b50c32cf
dannypsnl <dannypsnl@gmail.com>
2019-08-15 20:23:50 +0800

    extract Parser module and show expression

--- src/Main.hs
+++ src/Main.hs
@@ -1,76 +1,15 @@
 module Main where
-import System.Environment
-import Text.ParserCombinators.Parsec hiding (spaces)
-import Control.Monad (liftM)
+import Parser
+
+import qualified System.Environment as Env
+import qualified Text.ParserCombinators.Parsec as Parsec
 
 main :: IO ()
 main = do
-  (expr:_) <- getArgs
+  (expr:_) <- Env.getArgs
   putStrLn (readExpr expr)
 
-symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
-
 readExpr :: String -> String
-readExpr input = case parse parseExpr "lisp" input of
+readExpr input = case Parsec.parse parseExpr "lisp" input of
   Left err -> "no match: " ++ show err
-  Right val -> "found value"
-
-parseExpr :: Parser ScmValue
-parseExpr = parseAtom
-  <|> parseString
-  <|> parseNumber
-  <|> parseQuoted
-  <|> do char '('
-         x <- try parseList <|> parsePair
-         char ')'
-         return x
-
-spaces :: Parser ()
-spaces = skipMany1 space
-
-data ScmValue =
-  Atom String
-  | List [ScmValue]
-  | Pair [ScmValue] ScmValue
-  | Number Integer
-  | String String
-  | Bool Bool
-
-
-parseString :: Parser ScmValue
-parseString = do
-  char '"'
-  x <- many (noneOf "\"")
-  char '"'
-  return $ String x
-
-parseAtom :: Parser ScmValue
-parseAtom = do
-  first <- letter <|> symbol
-  rest <- many (letter <|> digit <|> symbol)
-  let atom = first:rest
-  return $ case atom of
-    "#t" -> Bool True
-    "#f" -> Bool False
-    _ -> Atom atom
-
-parseNumber :: Parser ScmValue
-parseNumber = do
-  num <- many1 digit
-  return $ Number (read num :: Integer)
-
-parseList :: Parser ScmValue
-parseList = liftM List $ sepBy parseExpr spaces
-
-parsePair :: Parser ScmValue
-parsePair = do
-    head <- endBy parseExpr spaces
-    tail <- char '.' >> spaces >> parseExpr
-    return $ Pair head tail
-
-parseQuoted :: Parser ScmValue
-parseQuoted = do
-    char '\''
-    x <- parseExpr
-    return $ List [Atom "quote", x]
+  Right val -> "found expression: " ++ show val
---
+++ src/Parser.hs
@@ -0,0 +1,82 @@
+
+module Parser (
+  parseExpr
+) where
+import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad (liftM)
+
+instance Show ScmValue where
+  show = showValue
+
+showValue :: ScmValue -> String
+showValue (String str) = "\"" ++ str ++ "\""
+showValue (Atom name) = name
+showValue (Number num) = show num
+showValue (Bool True) = "#t"
+showValue (Bool False) = "#f"
+showValue (List contents) = "(" ++ unwordsList contents ++ ")"
+showValue (Pair head tail) = "(" ++ unwordsList head ++ " . " ++ showValue tail ++ ")"
+
+unwordsList :: [ScmValue] -> String
+unwordsList = unwords . map showValue
+
+data ScmValue =
+    Atom String
+    | List [ScmValue]
+    | Pair [ScmValue] ScmValue
+    | Number Integer
+    | String String
+    | Bool Bool
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+parseExpr :: Parser ScmValue
+parseExpr = parseAtom
+  <|> parseString
+  <|> parseNumber
+  <|> parseQuoted
+  <|> do char '('
+         x <- try parseList <|> parsePair
+         char ')'
+         return x
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+parseString :: Parser ScmValue
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser ScmValue
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first:rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
+
+parseNumber :: Parser ScmValue
+parseNumber = do
+  num <- many1 digit
+  return $ Number (read num :: Integer)
+
+parseList :: Parser ScmValue
+parseList = liftM List $ sepBy parseExpr spaces
+
+parsePair :: Parser ScmValue
+parsePair = do
+    head <- endBy parseExpr spaces
+    tail <- char '.' >> spaces >> parseExpr
+    return $ Pair head tail
+
+parseQuoted :: Parser ScmValue
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]

0d1284881b07f490541be0805e19061e677fe579
dannypsnl <dannypsnl@gmail.com>
2019-08-16 11:39:38 +0800

    fix nix-build

--- little-scheme.nix
+++ little-scheme.nix
@@ -1,10 +1,15 @@
 { mkDerivation, base, stdenv }:
+let
+  pkgs = import <nixpkgs> { };
+  inherit (pkgs) haskellPackages;
+  parsec = haskellPackages.callHackage "parsec" "3.1.13.0" {};
+in
 mkDerivation {
   pname = "little-scheme";
   version = "0.1.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base ];
+  executableHaskellDepends = [ base parsec ];
   license = stdenv.lib.licenses.mit;
 }

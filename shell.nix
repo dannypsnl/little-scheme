8ea2b01034d97e021ab0422df987c960c8582c6e
dannypsnl <dannypsnl@gmail.com>
2019-09-07 23:18:14 +0800

    install stylish-haskell into shell

--- shell.nix
+++ shell.nix
@@ -3,6 +3,7 @@ let
   inherit (pkgs) mkShell;
   inherit (pkgs) haskellPackages;
   inherit (haskellPackages) cabal-install;
+  inherit (haskellPackages) stylish-haskell;
 
   ghc = haskellPackages.ghcWithPackages (pkgs: with pkgs; [base mtl]);
   dependencies = import ./deps.nix;
@@ -11,5 +12,6 @@ mkShell {
   buildInputs = [
     ghc
     cabal-install
+    stylish-haskell
   ] ++ dependencies;
 }

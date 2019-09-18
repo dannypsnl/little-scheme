287e537961bb666383cdfe9211dd99206166328a
dannypsnl <dannypsnl@gmail.com>
2019-09-18 13:56:17 +0800

    reduce functions in top scope

--- src/Main.hs
+++ src/Main.hs
@@ -17,27 +17,24 @@ main = do
     1 -> evalAndPrint $ head args
     _ -> putStrLn "Program only takes 0 or 1 argument"
 
-flushStr :: String -> IO ()
-flushStr str = putStr str >> hFlush stdout
-
-readPrompt :: String -> IO String
-readPrompt prompt = flushStr prompt >> getLine
-
-evalString :: String -> IO String
-evalString expr = return $ extractValue $ trapError (fmap show $ readExpr expr >>= eval)
+runRepl :: IO ()
+runRepl = until_ (== "quit") evalAndPrint
 
 evalAndPrint :: String -> IO ()
 evalAndPrint expr = evalString expr >>= putStrLn
+  where
+    evalString :: String -> IO String
+    evalString expr = return $ extractValue $ trapError (fmap show $ readExpr expr >>= eval)
 
-until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
-until_ pred prompt action = do
-  result <- prompt
+until_ :: (String -> Bool) -> (String -> IO ()) -> IO ()
+until_ pred action = do
+  result <- readPrompt "> "
   if pred result
     then return ()
-    else action result >> until_ pred prompt action
-
-runRepl :: IO ()
-runRepl = until_ (== "quit") (readPrompt "> ") evalAndPrint
+    else action result >> until_ pred action
+  where
+    readPrompt prompt = flushStr prompt >> getLine
+    flushStr str = putStr str >> hFlush stdout
 
 readExpr :: String -> ThrowsError ScmValue
 readExpr input = case parse parseExpr "scheme" input of

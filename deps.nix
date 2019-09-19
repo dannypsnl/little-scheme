6b7433124cfba7cec47773b6542147ff464255f1
dannypsnl <dannypsnl@gmail.com>
2019-09-20 00:03:52 +0800

    improve REPL via using Haskeline
    ref: https://github.com/judah/haskeline/wiki/KeyBindings

--- deps.nix
+++ deps.nix
@@ -2,7 +2,9 @@ let
   pkgs = import <nixpkgs> { };
   inherit (pkgs) haskellPackages;
   parsec = haskellPackages.callHackage "parsec" "3.1.14.0" {};
+  haskeline = haskellPackages.callHackage "haskeline" "0.7.5.0" {};
 in
 [
   parsec
+  haskeline
 ]
--- src/Main.hs
+++ src/Main.hs
@@ -2,39 +2,35 @@ module Main where
 import Interpreter (ScmError(..), ThrowsError, eval, extractValue, trapError)
 import Parser (ScmValue(..), parseExpr)
 
-import Control.Monad (liftM)
 import Control.Monad.Except (throwError)
+import Control.Monad.Trans (liftIO)
+import System.Console.Haskeline (InputT, defaultSettings, getInputLine, outputStrLn, runInputT)
 import System.Environment (getArgs)
-import System.IO (hFlush, stdout)
 import Text.Parsec (parse)
 
 main :: IO ()
 main = do
   args <- getArgs
-  case length args of
-    0 -> runRepl
+  case args of
+    [] -> runInputT defaultSettings repl
     -- directly eval the input
-    1 -> evalAndPrint $ head args
+    [expr] -> evalAndPrint expr
     _ -> putStrLn "Program only takes 0 or 1 argument"
 
-runRepl :: IO ()
-runRepl = until_ (== "quit") evalAndPrint
+type Repl a = InputT IO a
+
+repl :: Repl ()
+repl = do
+  minput <- getInputLine "> "
+  case minput of
+    Nothing -> outputStrLn "#bye"
+    Just input -> (liftIO $ evalAndPrint input) >> repl
 
 evalAndPrint :: String -> IO ()
 evalAndPrint expr = evalString expr >>= putStrLn
   where
     evalString :: String -> IO String
     evalString expr = return $ extractValue $ trapError (fmap show $ readExpr expr >>= eval)
-
-until_ :: (String -> Bool) -> (String -> IO ()) -> IO ()
-until_ pred action = do
-  result <- readPrompt "> "
-  if pred result
-    then return ()
-    else action result >> until_ pred action
-  where
-    readPrompt prompt = flushStr prompt >> getLine
-    flushStr str = putStr str >> hFlush stdout
 
 readExpr :: String -> ThrowsError ScmValue
 readExpr input = case parse parseExpr "scheme" input of

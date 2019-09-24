46fdbcce4fb2923ad8d4eaa0fb440656cfd98e27
dannypsnl <dannypsnl@gmail.com>
2019-09-24 21:21:50 +0800

    hope github actions cabal is greater than 1.8

--- default.nix
+++ default.nix
@@ -4,6 +4,6 @@ let
 in
 nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./little-scheme.nix {
   parsec = haskellPackages.callHackage "parsec" "3.1.14.0" {};
-  haskeline = haskellPackages.callHackage "haskeline" "0.7.5.0" {};
+  haskeline = haskellPackages.callHackage "haskeline" "0.7.4.3" {};
   hspec = haskellPackages.callHackage "hspec" "2.7.1" {};
 }

c5eaa59563779bf6e1b653a052e46b1b7b41ccc1
dannypsnl <dannypsnl@gmail.com>
2020-04-27 20:57:05 +0800

    (#45) swap to stack
    TODO: nix scripts

--- Setup.hs
+++ Setup.hs
@@ -1,26 +1,2 @@
-import Control.Monad
-import Data.Maybe
 import Distribution.Simple
-import System.Directory
-import System.Environment
-import System.FilePath
-
-main = do
-  defaultMain
-  inCI <- lookupEnv "GITHUB_WORKSPACE"
-  when (isNothing inCI) (do
-    putStrLn "Not CI environment, install scheme library"
-    initLittleScheme
-    putStrLn "scheme library installed"
-    )
-
-initLittleScheme :: IO ()
-initLittleScheme = do
-  target <- defaultLibraryPath
-  createDirectoryIfMissing True target
-  copyFile "lib/stdlib.scm" (target </> "stdlib.scm")
-
-defaultLibraryPath :: IO FilePath
-defaultLibraryPath = do
-  homeDir <- getHomeDirectory
-  return $ homeDir </> ".little-scheme" </> "lib"
+main = defaultMain

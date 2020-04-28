f36c54d807e777521003a55ff89e0f7ee205085e
dannypsnl <dannypsnl@gmail.com>
2020-04-28 20:33:39 +0800

    setup.hs should not be changed

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,26 @@
+import Control.Monad
+import Data.Maybe
 import Distribution.Simple
-main = defaultMain
+import System.Directory
+import System.Environment
+import System.FilePath
+
+main = do
+  defaultMain
+  inCI <- lookupEnv "GITHUB_WORKSPACE"
+  when (isNothing inCI) (do
+    putStrLn "Not CI environment, install scheme library"
+    initLittleScheme
+    putStrLn "scheme library installed"
+    )
+
+initLittleScheme :: IO ()
+initLittleScheme = do
+  target <- defaultLibraryPath
+  createDirectoryIfMissing True target
+  copyFile "lib/stdlib.scm" (target </> "stdlib.scm")
+
+defaultLibraryPath :: IO FilePath
+defaultLibraryPath = do
+  homeDir <- getHomeDirectory
+  return $ homeDir </> ".little-scheme" </> "lib"

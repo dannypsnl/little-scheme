b6566663e9021bb177eccb793355a7d8191b68ef
dannypsnl <dannypsnl@gmail.com>
2020-05-05 19:21:30 +0800

    (#44) stage0 to stage1 init

--- src/Scheme/Ast/Stage1.hs
+++ src/Scheme/Ast/Stage1.hs
@@ -1,15 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Scheme.Ast.Stage1 (
   Stage1(..)
+  , fromStage0
 ) where
+import Control.Monad.Except (throwError)
 import Data.Text
+import Scheme.Ast.Stage0
+import Scheme.Core (IOThrowsError, ScmError(..))
 import Text.Megaparsec.Pos
 
 data Stage1 =
-  Lambda SourcePos [String] [Stage1] -- (lambda parameter... clause...)
+  -- reuse
+  Stage0 Stage0
+  -- extract structure from s-expression
+  | Lambda SourcePos [String] [Stage1] -- (lambda parameter... clause...)
   | Define SourcePos
   | Let SourcePos [Stage1] [Stage1]
-  -- value
-  | Bool SourcePos Bool
-  | Atom SourcePos Text
-  | Number SourcePos Integer
-  | String SourcePos Text
+  deriving (Show, Eq)
+
+fromStage0 :: Stage0 -> IOThrowsError Stage1
+fromStage0 (List p ((Atom _ "lambda") : rest)) = return $ Lambda p [] []
+fromStage0 (List p ((Atom _ "define") : rest)) = return $ Define p
+fromStage0 (List p ((Atom _ "let") : rest)) = return $ Let p [] []
+fromStage0 (List p bad) = throwError $ Default $ "unknown form" ++ show bad
+fromStage0 stage0 = return $ Stage0 stage0

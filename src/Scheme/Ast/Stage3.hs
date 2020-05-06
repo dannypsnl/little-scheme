b056afd5a930d191f40aa6d03101a08c5afb1523
dannypsnl <dannypsnl@gmail.com>
2020-05-06 18:47:03 +0800

    (#44) stage3: let to lambda

--- src/Scheme/Ast/Stage3.hs
+++ src/Scheme/Ast/Stage3.hs
@@ -13,16 +13,24 @@ data Stage3 = Stage0_3 Stage0
   | Lambda_3 SourcePos [Variable] [Stage3]
   | Define_3 SourcePos Text (Maybe [Variable]) [Stage3]
   | Set_3 SourcePos Text Stage3
-  | Binding_3 SourcePos Text Stage3
   | If_3 SourcePos Stage3 Stage3 Stage3
   | Application_3 SourcePos [Stage3]
   deriving (Show, Eq)
 
 -- transform `let` to `lambda`
 toStage3 :: Stage2 -> IOThrowsError Stage3
-toStage3 (Let_2 a b c) = do
-  -- TODO
-  return $ Lambda_3 a [] []
+toStage3 (Let_2 pos bindings body) = do
+  params <- mapM paramFromBinding bindings
+  args <- mapM argFromBinding bindings
+  body <- mapM toStage3 body
+  return $ (Application_3 pos (Lambda_3 pos params body : args))
+  where
+    paramFromBinding :: Stage2 -> IOThrowsError Variable
+    paramFromBinding (Binding_2 pos param _) = return $ Variable pos param
+    argFromBinding :: Stage2 -> IOThrowsError Stage3
+    argFromBinding (Binding_2 _ _ init) = do
+      arg <- toStage3 init
+      return $ arg
 -- copy
 toStage3 (Stage0_2 stage0) = return $ Stage0_3 stage0
 toStage3 (Lambda_2 a b c) = do
@@ -34,9 +42,6 @@ toStage3 (Define_2 a b c d) = do
 toStage3 (Set_2 a b c) = do
   c <- toStage3 c
   return $ Set_3 a b c
-toStage3 (Binding_2 a b c) = do
-  c <- toStage3 c
-  return $ Binding_3 a b c
 toStage3 (If_2 a b c d) = do
   b <- toStage3 b
   c <- toStage3 c
---
+++ test/Ast/Stage3Spec.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Ast.Stage3Spec where
+import Control.Monad.Except (runExceptT)
+import SpecHelper
+import Text.Megaparsec.Pos
+
+import Scheme.Ast.Stage0
+import Scheme.Ast.Stage1
+import Scheme.Ast.Stage2
+import Scheme.Ast.Stage3
+
+spec :: Spec
+spec = describe "transform" $ do
+  context "stage2 -> stage3" $ do
+    it "transform reuse part" $ do
+      (Stage0_2 (Atom pos "aaa")) `transResultIs` (Stage0_3 (Atom pos "aaa"))
+    it "let -> lambda" $ do
+      (Let_2 pos [Binding_2 pos "a" (Stage0_2 (Number pos 1))]
+                 [Stage0_2 (Atom pos "a")])
+        `transResultIs`
+        (Application_3 pos [Lambda_3 pos [Variable pos "a"] [Stage0_3 (Atom pos "a")], Stage0_3 (Number pos 1)])
+  where
+    transResultIs stage2 expectedStage3 = (runExceptT $ toStage3 stage2) >>= (`shouldBe` Right expectedStage3)
+
+-- pos is dummy value
+pos :: SourcePos
+pos = (SourcePos "" pos1 pos1)

9b6ca2fcbfc5f10e824668cfed13205e50d1c0c9
dannypsnl <dannypsnl@gmail.com>
2020-05-06 11:47:25 +0800

    remove outdated nix scripts for now

--- default.nix
+++
@@ -1,9 +0,0 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc865" }:
-let
-  inherit (nixpkgs) haskellPackages;
-in
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./little-scheme.nix {
-  parsec = haskellPackages.callHackage "parsec" "3.1.14.0" {};
-  haskeline = haskellPackages.callHackage "haskeline" "0.7.4.3" {};
-  hspec = haskellPackages.callHackage "hspec" "2.7.1" {};
-}

a2acc1214d43bbfb3a2a4ec1496f7b309da48895
dannypsnl <dannypsnl@gmail.com>
2020-05-08 20:27:20 +0800

    (#44) NI: new environment, Monad, new error and new apply

--- src/Scheme/Ast/Core.hs
+++ src/Scheme/Ast/Core.hs
@@ -1,12 +1,28 @@
+{-# LANGUAGE RecordWildCards #-}
 module Scheme.Ast.Core (
   Core(..)
+  , getPos
   , toCore
+  , Runtime(..)
+  , Env
+  , EnvImpl(..)
+  , setVar, getVar, defineVar
+  , ScmError(..)
+  , IOThrowsError, ThrowsError
+  , liftThrows
 ) where
-import Data.Text hiding (head, tail)
+import Control.Monad.Except
+import Control.Monad.Trans
+import Data.IORef
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Maybe
+import Data.Text (Text)
+import qualified Data.Text as T
 import Scheme.Ast.Stage0
 import Scheme.Ast.Stage1
 import Scheme.Ast.Stage4
-import Scheme.Core hiding (ScmValue(..))
+import System.IO (Handle)
 import Text.Megaparsec.Pos
 
 data Core = CoreNumber SourcePos Integer
@@ -18,6 +34,25 @@ data Core = CoreNumber SourcePos Integer
   | CoreSet SourcePos Text Core
   | CoreIf SourcePos Core Core Core
   | CoreApplication SourcePos Core [Core]
+  -- runtime
+  | CoreRuntime SourcePos Runtime
+
+getPos :: Core -> SourcePos
+getPos (CoreNumber pos _) = pos
+getPos (CoreBool pos _) = pos
+getPos (CoreString pos _) = pos
+getPos (CoreQouted pos _) = pos
+getPos (CoreLambda pos _ _) = pos
+getPos (CoreDefine pos _ _) = pos
+getPos (CoreSet pos _ _) = pos
+getPos (CoreIf pos _ _ _) = pos
+getPos (CoreApplication pos _ _) = pos
+getPos (CoreRuntime pos _) = pos
+
+data Runtime = PrimitiveFunc ([Core] -> ThrowsError Core)
+  | Func [Text] (Maybe Text) [Core] Env
+  | IOFunc ([Core] -> IOThrowsError Core)
+  | Port Handle
 
 toCore :: Stage4 -> IOThrowsError Core
 toCore (Lambda_4 pos params body) = do
@@ -37,3 +72,115 @@ toCore (If_4 pos pred thenE elseE) = do
 toCore (Application_4 pos expressions) = do
   exprs <- mapM toCore expressions
   return $ CoreApplication pos (head exprs) (tail exprs)
+
+-- Env
+data EnvImpl = EnvImpl {
+  env :: Map Text (IORef Core)
+  , parentEnv :: Maybe Env }
+type Env = IORef EnvImpl
+
+getVar :: Env -> SourcePos -> Text -> IOThrowsError Core
+getVar envRef pos var = do
+  EnvImpl { env=env, parentEnv=parentEnv } <- liftIO $ readIORef envRef
+  case parentEnv of
+    Nothing ->
+      maybe (throwError $ UnboundVar pos "Can not found variable" (T.unpack var))
+        (liftIO . readIORef)
+        (Map.lookup var env)
+    Just parent -> getVar parent pos var
+
+setVar :: Env -> SourcePos -> Text -> Core -> IOThrowsError Core
+setVar envRef pos var val = do
+  EnvImpl { env=env, parentEnv=parentEnv } <- liftIO $ readIORef envRef
+  case parentEnv of
+    Nothing ->
+      maybe (throwError $ UnboundVar pos "Can not set an unbound variable" (T.unpack var))
+        (liftIO . readIORef)
+        (Map.lookup var env)
+    Just parent -> setVar parent pos var val
+
+defineVar :: Env -> SourcePos -> Text -> Core -> IOThrowsError Core
+defineVar envRef pos var val = do
+  isDefined <- liftIO $ isBound envRef var
+  if isDefined
+    then setVar envRef pos var val >> return val
+    else liftIO $ do
+      valueRef <- newIORef val
+      env'@EnvImpl{env=env, ..} <- readIORef envRef
+      writeIORef envRef env' { env=(Map.insert var valueRef env) }
+      return val
+  where
+    isBound :: Env -> Text -> IO Bool
+    isBound env var = do
+      EnvImpl { env=env, .. } <- readIORef env
+      return $ isJust (Map.lookup var env)
+
+-- show position
+showPos :: SourcePos -> String
+showPos (SourcePos {sourceName=sourceName, sourceLine=sourceLine, sourceColumn=sourceColumn}) =
+  sourceName ++ ":" ++ (show sourceLine) ++ ":" ++ (show sourceColumn) ++ ":"
+
+-- show core
+instance Eq Core where
+  (==) l r = show l == show r
+  (/=) l r = show l /= show r
+
+instance Show Core where
+  show = showValue
+
+showValue :: Core -> String
+showValue (CoreString _ str) = "\"" ++ (T.unpack str) ++ "\""
+showValue (CoreNumber _ num) = show num
+showValue (CoreBool _ True) = "#t"
+showValue (CoreBool _ False) = "#f"
+showValue (CoreRuntime _ (PrimitiveFunc _)) = "<primitive>"
+showValue (CoreRuntime _  (Func args varargs _ _)) =
+   "(lambda (" ++ unwords (map show args) ++
+      (case varargs of
+         Nothing -> ""
+         Just arg -> " . " ++ (T.unpack arg)) ++ ") ...)"
+showValue (CoreRuntime _ (Port _)) = "<IO port>"
+showValue (CoreRuntime _ (IOFunc _)) = "<IO primitive>"
+
+unwordsList :: [Core] -> String
+unwordsList = unwords . map showValue
+-- error
+data ScmError =
+  -- NumArgs expected-number-of-arguments list-of-parameters-actual-got
+  NumArgs SourcePos Integer [Core]
+  -- TypeMismatch type-string-format value-actual-got
+  --
+  -- example:
+  --   TypeMismatch "string" bad
+  | TypeMismatch SourcePos String Core
+  | BadSpecialForm SourcePos String Core
+  | NotFunction SourcePos String Core
+  | UnboundVar SourcePos String String
+  | Default SourcePos String
+  | NonExhaustivePattern SourcePos [Core]
+
+
+instance Eq ScmError where
+  (==) l r = show l == show r
+  (/=) l r = show l /= show r
+
+showError :: ScmError -> String
+showError (UnboundVar pos message varname) = (showPos pos) ++ message ++ ": " ++ varname
+showError (BadSpecialForm pos message form) = (showPos pos) ++ message ++ ": " ++ show form
+showError (NotFunction pos message func) = (showPos pos) ++ message ++ ": " ++ show func
+showError (NumArgs pos expected found) = (showPos pos) ++ "Expected " ++ show expected
+                                       ++ " args; found values " ++ unwordsList found
+showError (TypeMismatch pos expected found) = (showPos pos) ++ "Invalid type: expected " ++ expected
+                                       ++ ", found " ++ show found
+showError (NonExhaustivePattern pos clauses) = (showPos pos) ++ "Non-exhaustive patterns: " ++ unwordsList clauses
+showError (Default pos message) = (showPos pos) ++ "Error: " ++ message
+
+instance Show ScmError where show = showError
+
+type IOThrowsError = ExceptT ScmError IO
+
+liftThrows :: ThrowsError a -> IOThrowsError a
+liftThrows (Left err) = throwError err
+liftThrows (Right val) = return val
+
+type ThrowsError = Either ScmError
--- src/Scheme/NewInterpreter.hs
+++ src/Scheme/NewInterpreter.hs
@@ -1,14 +1,10 @@
 module Scheme.NewInterpreter () where
 import Control.Monad.Except
 import Control.Monad.Trans
-import Data.IORef
-import Data.Map.Strict
-import qualified Data.Map.Strict as Map
 import Data.Maybe
-import Data.Text
-import qualified Data.Text as T
+import Data.Text hiding (drop, last, length, map, zip)
 import Scheme.Ast.Core
-import Scheme.Core (IOThrowsError, ScmError(..))
+import Text.Megaparsec.Pos
 
 -- eval
 evalCore :: Env -> Core -> IOThrowsError Core
@@ -17,39 +13,40 @@ evalCore env v@(CoreBool _ _) = return v
 evalCore env v@(CoreString _ _) = return v
 evalCore env (CoreDefine pos name expr) = do
   expr <- evalCore env expr
-  r <- defineVar env name expr
+  r <- defineVar env pos name expr
   return $ r
 evalCore env (CoreSet pos name expr) = do
   expr <- evalCore env expr
-  r <- setVar env name expr
+  r <- setVar env pos name expr
   return $ r
 evalCore env (CoreIf pos pred thenE elseE) = do
   result <- evalCore env pred
   case result of
     CoreBool _ True -> evalCore env thenE
     _ -> evalCore env elseE
+evalCore env (CoreApplication pos f args) = do
+  f' <- evalCore env f
+  args <- mapM (evalCore env) args
+  apply f' args
 
--- Env
-type Env = IORef (Map Text (IORef Core))
+-- produce runtime function
+makeFunc :: Monad m => SourcePos -> Maybe Text -> Env -> [Core] -> [Core] -> m Core
+-- FIXME: correct params
+makeFunc pos varargs env params body = return $ CoreRuntime pos (Func [] varargs body env)
+makeNormalFunc :: Env -> SourcePos -> [Core] -> [Core] -> IOThrowsError Core
+makeNormalFunc env pos = makeFunc pos Nothing env
 
-setVar :: Env -> Text -> Core -> IOThrowsError Core
-setVar envRef var val = do
-  env <- liftIO $ readIORef envRef
-  maybe (throwError $ UnboundVar "Can not set an unbound variable" (T.unpack var))
-        (liftIO . (`writeIORef` val))
-        (Map.lookup var env)
-  return val
-
-defineVar :: Env -> Text -> Core -> IOThrowsError Core
-defineVar envRef var val = do
-  isDefined <- liftIO $ isBound envRef var
-  if isDefined
-    then setVar envRef var val >> return val
-    else liftIO $ do
-      valueRef <- newIORef val
-      env <- readIORef envRef
-      writeIORef envRef (Map.insert var valueRef env)
-      return val
-  where
-    isBound :: Env -> Text -> IO Bool
-    isBound env var = isJust . Map.lookup var <$> readIORef env
+-- application
+apply :: Core -> [Core] -> IOThrowsError Core
+apply (CoreRuntime pos (PrimitiveFunc f)) args = liftThrows $ f args
+apply (CoreRuntime pos (Func params vararg body closure)) args =
+  if length params /= length args && isNothing vararg
+    then throwError $ NumArgs pos (toInteger $ length params) args
+    else do
+      -- bind params with args
+      mapM (\(var, val) -> defineVar closure pos var val) (zip params args)
+      evalBody closure body
+    where
+      evalBody env body = last <$> mapM (evalCore env) body
+apply (CoreRuntime pos (IOFunc f)) args = f args
+apply bad _ = throwError $  NotFunction (getPos bad) "Applying a not function value" bad
